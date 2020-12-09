ec7786dd80e1c1ab7254bc82a9ae6575777756c2
Anderson Schwertner <andersonschwertner@hotmail.com>
2020-12-08 21:20:08 -0300

    First version of auxiliary functions

---
+++ src/auxiliary_functions.jl
@@ -0,0 +1,216 @@
+using LinearAlgebra
+
+"""
+
+    active_set(gk, xk, a, b)
+
+    Constructs the set of active constraints
+
+    - 'gk': n-dimensional vector (gradient of the model calculated in xk)
+    - 'xk': n-dimensional vector (current iterate)
+    - 'a': n-dimensional vector with the lower bounds
+    - 'b': n-dimensional vector with the upper bounds
+
+    Returns a list with the indices that are fixed at the bounds
+
+"""
+function active_set(gk, xk, a, b)
+    n = length(xk)
+    index_list = []
+
+    for i=1:n
+        if ( xk[i] == a[i] && gk[i] >= 0.0 ) || ( xk[i] == b[i] && gk[i] <= 0.0 )
+            push!(index_list, i)
+        end
+    end
+
+    return index_list
+
+end
+
+"""
+
+    projection_active_set(v, index_set)
+
+    Constructs the projection operator from the set of active constraints
+    
+    - 'v': n-dimensional vector 
+    - 'index_set': list with the indices of the active constraints
+
+    Returns a n-dimensional vector, which is the projection of v by the set of active constraints
+
+"""
+function projection_active_set(v, index_set)
+    n = length(index_set)
+    proj_v = copy(v)
+
+    for i=1:n
+        proj_v[index_set[i]] = 0.0
+    end
+
+    return proj_v
+
+end
+
+"""
+
+    alpha_Delta(d, s, Δ)
+
+    Determines which is the largest value of α such that ||d + αs|| <= Δ is satisfied
+
+    - 'd': n-dimensional vector (direction) 
+    - 's': n-dimensional vector (new search-direction) 
+    - 'Δ': positive real value (trust-region radius)
+
+    Returns the real value α_Δ
+
+"""
+function alpha_Delta(d, s, Δ)
+    ds = dot(d, s)
+    ss = dot(s, s)
+    dd = dot(d, d)
+
+    r_1 = (ds - sign(ds) * sqrt(ds ^ 2.0 - ss * (dd - Δ ^ 2.0)) ) / ss
+    r_2 = (dd - Δ ^ 2.0) / (r_1 * ss)
+
+    return max(r_1, r_2)
+
+end
+
+"""
+
+    alpha_B(xk, d, s, a, b)
+
+    Determines which is the largest value of α such that xk + d + αs satisfies the bounds
+
+    - 'xk': n-dimensional vector (current iterate)
+    - 'd': n-dimensional vector (direction) 
+    - 's': n-dimensional vector (new search-direction)    
+    - 'a': n-dimensional vector with the lower bounds
+    - 'b': n-dimensional vector with the upper bounds
+
+    Returns the real value α_B and the index for which the bound is achieved
+
+"""
+function alpha_B(xk, d, s, a, b)
+    n = length(xk)
+    α = Inf
+    α_i = 0.0
+    index = 0
+
+    for i=1:n
+        if s[i] > 0.0
+            α_i = (b[i] - x[i] - d[i]) / s[i]
+        elseif s[i] < 0.0
+            α_i = (a[i] - x[i] - d[i]) / s[i]
+        else
+            α_i = Inf
+        end
+        if α_i < α
+            α = α_i
+            index = i
+        end
+    end
+
+    return α, index
+
+end
+
+"""
+
+    alpha_Q(gk, Gk, d, s)
+
+    Determines which is the largest value of α such that Q(xk + d + αs) decreases monotonically
+
+    - 'gk': n-dimensional vector (gradient of the model calculated in xk)
+    - 'Gk': n × n matrix (hessian of the model calculated in xk)
+    - 'd': n-dimensional vector (direction) 
+    - 's': n-dimensional vector (new search-direction)    
+
+    Returns the real value α_Q
+
+"""
+function alpha_Q(gk, Gk, d, s)
+    sGd = dot(s, Gk * d)
+    sGs = dot(s, Gk * s)
+    sg = dot(s, gk)
+
+    α_Q = Inf
+
+    if sGs > 0.0
+        α_Q = (- sg - sGd) / sGs
+    end
+
+    return α_Q
+
+end
+
+"""
+
+    calculate_alpha(gk, Gk, xk, d, s, Δ, a, b)
+
+    Determines the stepsize α, which will be the minimum value between α_Δ, α_B and α_Q
+
+    - 'gk': n-dimensional vector (gradient of the model calculated in xk)
+    - 'Gk': n × n matrix (hessian of the model calculated in xk)
+    - 'xk': n-dimensional vector (current iterate) 
+    - 'd': n-dimensional vector (direction) 
+    - 's': n-dimensional vector (new search-direction)
+    - 'Δ': positive real value (trust-region radius)
+    - 'a': n-dimensional vector with the lower bounds
+    - 'b': n-dimensional vector with the upper bounds    
+    
+    Returns the real value α, an index that indicates which value was chosen, and the index
+    for which the bound is achieved (in the case α = α_B) or 0 for the other cases
+
+"""
+function calculate_alpha(gk, Gk, xk, d, s, Δ, a, b)
+    v = zeros(3)
+    v[1] = alpha_Delta(d, s, Δ)
+    v[2], index = alpha_B(xk, d, s, a, b)
+    v[3] = alpha_Q(gk, Gk, d, s)
+    values = findmin(v)
+    if values[2] != 2
+        index = 0
+    end
+
+    return values[1], values[2], index
+
+end
+
+"""
+
+    new_search_direction(proj_d, proj_grad)
+
+    Calculate a new search direction for the case α = α_Δ
+
+    - 'proj_d': n-dimensional vector (projection of the direction d)
+    - 'proj_grad': n-dimensional vector (projection of gradient of the model calculated in xk + d) 
+    
+    Returns a n-dimensional vector
+
+"""
+function new_search_direction(proj_d, proj_grad)
+    dg = dot(proj_d, proj_grad)
+    dd = dot(proj_d, proj_d)
+    gg = dot(proj_grad, proj_grad)
+    α = 0.0
+    β = 0.0
+    aux = 0.0
+
+    if dg == 0
+        β = - sqrt(dd / gg)
+    else
+        aux = ((dd ^ 2.0 * gg) / (dg ^ 2.0)) - dd
+        α = - sqrt(aux * dd) / aux
+        β = - α * dd / dg
+
+        if β >= - α * dg / gg
+            α = dd / sqrt(aux * dd)
+            β = - α * dd / dg
+        end
+    end
+
+    return α * proj_d + β * proj_grad
+    
+end
---
+++ src/trsbox.jl
@@ -0,0 +1,102 @@
+# TRSBOX: Versão inicial
+
+using LinearAlgebra
+
+include("auxiliary_functions.jl")
+
+"""
+
+    tcg_active_set(gk, Gk, xk, Δ, a, b)
+
+    A version of a Truncated Conjugate Gradient with Active Set algorithm for the
+    Trust-Region Subproblem
+
+    - 'gk': n-dimensional vector (gradient of the model calculated in xk)
+    - 'Gk': (n × n)-dimensional matrix (hessian of the model calculated in xk)
+    - 'xk': n-dimensional vector (current iterate) 
+    - 'Δ': positive real value (trust-region radius)
+    - 'a': n-dimensional vector with the lower bounds
+    - 'b': n-dimensional vector with the upper bounds 
+    
+    Returns a n-dimensional vector.
+
+"""
+function tcg_active_set(gk, Gk, xk, Δ, a, b)
+    n = length(xk)
+    α = 0.0 
+    index_α = 0 
+    index = 0
+    aux_1 = 0.0
+    aux_2 = 0.0
+    aux_3 = 0.0
+    aux_4 = 0.0
+    aux_vector_1 = zeros(n)
+    aux_vector_2 = zeros(n)
+    aux_vector_3 = zeros(n)
+    aux_vector_4 = zeros(n)
+
+    I = active_set(gk, xk, a, b)
+    d = zeros(n)
+    s = - projection_active_set(gk, I)
+
+    while true      
+
+        if length(I) == n
+            return d
+        end
+
+        α, index_α, index = calculate_alpha(gk, Gk, xk, d, s, Δ, a, b)
+
+        d .= d .+ α[1] .* s
+
+        mul!(aux_vector_1, Gk, d)
+        aux_vector_1 .+= gk 
+        aux_vector_2 .= projection_active_set(aux_vector_1, I)
+        aux_1 = dot(aux_vector_2, aux_vector_2)
+        aux_2 = dot(d, gk) + dot(d, Gk * d) / 2.0
+        
+        # α_Delta is chosen
+        if index_α == 1
+            aux_vector_4 .= projection_active_set(d, I)
+            aux_4 = dot(aux_vector_4, aux_vector_4)
+            
+            if aux_4 * aux_1 - dot(aux_vector_4, aux_vector_2) ^ 2.0 + 1.0e-4 * aux_2 <= 0.0
+                return d
+            else
+                s .= new_search_direction(aux_vector_4, aux_vector_2)
+                #Ver
+            end
+
+        end
+
+        # α_B is chosen
+        if index_α == 2
+            push!(I, index)
+            
+            if sqrt(aux_1) * Δ + 1.0e-2 * aux_2 <= 0.0
+                return d
+            else
+                s .= .- aux_vector_2
+                continue
+            end
+
+        end
+
+        # α_Q is chosen
+        if index_α == 3
+            aux_vector_3 .= projection_active_set(gk, I)
+            aux_3 = dot(aux_vector_3, aux_vector_3)
+
+            if (sqrt(aux_1) * Δ + 1.0e-2 * aux_2 <= 0.0) || (sqrt(aux_1) - sqrt(aux_3) + 1.0e-2 * aux_2 <= 0.0)
+                return d
+            else
+                #Ver
+            end
+
+        end
+
+    end
+
+    return d
+end
+
