98b64e49612b376842d30ba10a105f99c6eb0eff
Anderson Schwertner <andersonschwertner@hotmail.com>
2020-12-17 21:49:54 -0300

    Calculate alpha_values and beta_values

--- src/preliminary_calculations.jl
+++ src/preliminary_calculations.jl
@@ -62,9 +62,10 @@ end
 
 """
 
-    construct_set!(x0, Δ, a, b, p, set)
+    construct_set!(x0, Δ, a, b, p, set, α_values, β_values)
 
-    Partially builds the set of interpolation points of the first model
+    Partially builds the set of interpolation points of the first model 
+    and two vectors with some important constants.
 
     - 'x0': n-dimensional vector (first iterate)
     - 'Δ': positive real value (trust-region radius)
@@ -72,29 +73,37 @@ end
     - 'b': n-dimensional vector with the upper bounds
     - 'p': integer (number of pairs of points to be generated)
     - 'set': n × m matrix (set of interpolation points)
+    - 'α_values': n-dimensional vector with some constants related with the first n points
+    - 'β_values': n-dimensional vector with some constants related with the last n points
 
-    Returns a modified version of the matrix set
+    Returns a modified version of the matrix set and two other vectors.
 
 """
-function construct_set!(x0, Δ, a, b, p, set)
+function construct_set!(x0, Δ, a, b, p, set, α_values, β_values)
     n = length(x0)
 
     for i=1:p
         if x0[i] == a[i]
             set[:, i + 1] = x0
             set[i, i + 1] += Δ
+            α_values[i] = Δ
             set[:, n + i + 1] = x0
             set[i, n + i + 1] += 2.0 * Δ
+            β_values[i] = 2.0 * Δ
         elseif x0[i] == b[i]
             set[:, i + 1] = x0
             set[i, i + 1] += -Δ
+            α_values[i] = -Δ
             set[:, n + i + 1] = x0
             set[i, n + i + 1] += -2.0 * Δ
+            β_values[i] = -2.0 * Δ
         else
             set[:, i + 1] = x0
             set[i, i + 1] += Δ
+            α_values[i] = Δ
             set[:, n + i + 1] = x0
             set[i, n + i + 1] += -Δ
+            β_values[i] = -Δ
         end
     end
 
@@ -102,9 +111,10 @@ end
 
 """
 
-    construct_set_aux!(x0, Δ, a, b, q, set)
+    construct_set_aux!(x0, Δ, a, b, q, set, α_values, β_values)
 
-    Changes the qth point of the set of interpolation points
+    Changes the qth point of the set of interpolation points and the qth value of the 
+    vectors α_values and β_values
 
     - 'x0': n-dimensional vector (first iterate)
     - 'Δ': positive real value (trust-region radius)
@@ -112,21 +122,29 @@ end
     - 'b': n-dimensional vector with the upper bounds
     - 'q': point index to be changed
     - 'set': n × m matrix (set of interpolation points)
+    - 'α_values': n-dimensional vector with some constants related with the first n points
+    - 'β_values': n-dimensional vector with some constants related with the last n points
 
-    Returns a modified version of the matrix set
+    Returns a modified version of the matrix set and two other vectors.
 
 """
-function construct_set_aux!(x0, Δ, a, b, q, set)
+function construct_set_aux!(x0, Δ, a, b, q, set, α_values, β_values)
 
     if x0[q] == a[q]
         set[:, q + 1] = x0
         set[q, q + 1] += Δ
+        α_values[q] = Δ
+        β_values[q] = 0.0
     elseif x0[p] == b[p]
         set[:, q + 1] = x0
         set[q, q + 1] += -Δ
+        α_values[q] = -Δ
+        β_values[q] = 0.0
     else
         set[:, q + 1] = x0
         set[q, q + 1] += Δ
+        α_values[q] = Δ
+        β_values[q] = 0.0
     end
 
 end
@@ -135,8 +153,9 @@ end
 
     initial_set(f, x0, Δ, a, b, m)
 
-    Builds the set of interpolation points of the first model and
-    compute the function values in these points
+    Builds the set of interpolation points of the first model,
+    compute the function values in these points and some important constants
+    relative to the interpolation points
     
     - 'f': objective function
     - 'x0': n-dimensional vector (first iterate)
@@ -145,12 +164,14 @@ end
     - 'b': n-dimensional vector with the upper bounds
     - 'm': integer (number of interpolation conditions)
 
-    Returns a n × m matrix, and a m-dimensional vector
+    Returns a n × m matrix, a m-dimensional vector and two n-dimensional vectors
 
 """
 function initial_set(f, x0, Δ, a, b, m)
     n = length(x0)
     set = zeros(n, m)
+    α_values = zeros(n)
+    β_values = zeros(n)
     f_values = zeros(m)
     set[:, 1] = x0
     aux_vector = zeros(n)
@@ -161,11 +182,11 @@ function initial_set(f, x0, Δ, a, b, m)
     if m <= (2*n + 1)
         if mod(m-1, 2)
             p = convert(Int, (m - 1) / 2)
-            construct_set!(x0, Δ, a, b, p, set)
+            construct_set!(x0, Δ, a, b, p, set, α_values, β_values)
         else
             p = convert(Int, (m - 2) / 2)
-            construct_set!(x0, Δ, a, b, p, set)
-            construct_set_aux!(x0, Δ, a, b, p + 1, set)
+            construct_set!(x0, Δ, a, b, p, set, α_values, β_values)
+            construct_set_aux!(x0, Δ, a, b, p + 1, set, α_values, β_values)
         end
 
         for i=1:m
@@ -173,7 +194,7 @@ function initial_set(f, x0, Δ, a, b, m)
         end
 
     else
-        construct_set!(x0, Δ, a, b, n, set)
+        construct_set!(x0, Δ, a, b, n, set, α_values, β_values)
         for i=1:convert(Int, 2 * n + 1)
             f_values[i] = f(set[:, i])
         end
@@ -186,6 +207,9 @@ function initial_set(f, x0, Δ, a, b, m)
                 aux = f_values[i + 1]
                 f_values[i + 1] = f_values[n + i + 1]
                 f_values[i + 1] = aux
+                aux = α_values[i]
+                α_values[i] = β_values[i]
+                β_values[i] = aux
             end
         end
 
@@ -222,6 +246,7 @@ function initial_set(f, x0, Δ, a, b, m)
         
     end
  
-    return set, f_values
+    return set, f_values, α_values, β_values
 
 end
+
