92431f9ddbfeea7fe222e992007e05af0d7b808c
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-01-20 19:19:37 -0300

    Add references

--- src/preliminary_calculations.jl
+++ src/preliminary_calculations.jl
@@ -1,4 +1,8 @@
-using LinearAlgebra
+# Preliminary functions for BOBYQA algorithm
+
+# Main reference:
+# POWELL, M. J. D. (2009). The BOBYQA algorithm for bound constrained optimization without derivatives.
+# Cambridge NA Report NA2009/06, University of Cambridge, Cambridge, 26-46.
 
 """
 
