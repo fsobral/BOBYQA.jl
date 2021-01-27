368246d51bd60f7c2eec0433a83deea4eee1497a
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-01-27 14:16:48 -0300

    remove some packages

--- src/trsbox.jl
+++ src/trsbox.jl
@@ -4,10 +4,6 @@
 # POWELL, M. J. D. (2009). The BOBYQA algorithm for bound constrained optimization without derivatives.
 # Cambridge NA Report NA2009/06, University of Cambridge, Cambridge.
 
-using LinearAlgebra
-
-include("auxiliary_functions.jl")
-
 """
 
     trsbox!(n, gk, Gk, xk, Δ, a, b, d)
