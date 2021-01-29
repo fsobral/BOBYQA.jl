78c2736eea36ee97a5e35a95dd779a539d710373
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-01-28 22:06:47 -0300

    adds tests to trsbox! function

--- test/test_trsbox.jl
+++ test/test_trsbox.jl
@@ -8,7 +8,7 @@
         x = ones(n)
         Δ = 5.0
         a = ones(n)
-        b = 2 * ones(n)
+        b = 2.0 * ones(n)
         d = rand(n)
 
         msg = BOBYQA.trsbox!(n, g, G, x, Δ, a, b, d)
@@ -21,9 +21,9 @@
     @testset "Exit condition: Initial search direction" begin
 
         n = 2    
-        g = zeros(n)
+        g = [1.0, 0.0]
         G = Matrix{Float64}(I, 3, 3)
-        x = 2.0 * ones(n)
+        x = [1.0, 2.0]
         Δ = 5.0
         a = ones(n)
         b = 3.0 * ones(n)
@@ -50,6 +50,22 @@
 
         @test(isapprox(d, - ones(n), atol = 1.0e-1))
         @test(msg == "Stopping criterion for α_B has been reached")
+
+    end
+
+    @testset "Exit condition: α_Q criterion" begin
+        
+        n = 2    
+        g = ones(n)
+        G = Matrix{Float64}(I, 3, 3)
+        x = 2.0 * ones(n)
+        Δ = 5.0
+        a = - ones(n)
+        b = 3.0 * ones(n)
+        d = rand(n)
+
+        @test(isapprox(d, - ones(n), atol = 1.0e-1))
+        @test(msg == "Stopping criterion for α_Q has been reached")
 
     end
 
