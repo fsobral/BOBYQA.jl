9b4f64d17b9fcf3c38159fb503db345c97626fbd
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-01-29 11:31:52 -0300

    correct tests for trsbox! function

--- test/test_trsbox.jl
+++ test/test_trsbox.jl
@@ -41,12 +41,14 @@
         
         n = 2    
         g = ones(n)
-        G = Matrix{Float64}(I, 3, 3)
+        G = Matrix{Float64}(I, n, n)
         x = 2.0 * ones(n)
         Δ = 5.0
         a = ones(n)
         b = 3.0 * ones(n)
-        d = rand(n)
+        d = zeros(n)
+
+        msg = BOBYQA.trsbox!(n, g, G, x, Δ, a, b, d)
 
         @test(isapprox(d, - ones(n), atol = 1.0e-1))
         @test(msg == "Stopping criterion for α_B has been reached")
@@ -57,12 +59,14 @@
         
         n = 2    
         g = ones(n)
-        G = Matrix{Float64}(I, 3, 3)
+        G = Matrix{Float64}(I, n, n)
         x = 2.0 * ones(n)
         Δ = 5.0
         a = - ones(n)
         b = 3.0 * ones(n)
         d = rand(n)
+
+        msg = BOBYQA.trsbox!(n, g, G, x, Δ, a, b, d)
 
         @test(isapprox(d, - ones(n), atol = 1.0e-1))
         @test(msg == "Stopping criterion for α_Q has been reached")
