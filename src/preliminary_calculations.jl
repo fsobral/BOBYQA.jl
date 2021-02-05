edaec8961dd960eadf9688e318f8d2d441b43c48
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-02-05 09:02:21 -0300

    modifies check_initial_room function

--- src/preliminary_calculations.jl
+++ src/preliminary_calculations.jl
@@ -6,10 +6,11 @@
 
 """
 
-    check_initial_room(Δ, a, b)
+    check_initial_room(n, Δ, a, b)
 
     Checks whether the limits satisfy the conditions b[i] >= a[i]+2*Δ
 
+    - 'n': dimension of the search space
     - 'Δ': positive real value (trust-region radius)
     - 'a': n-dimensional vector with the lower bounds
     - 'b': n-dimensional vector with the upper bounds
@@ -17,8 +18,7 @@
     Returns a boolean (true if satisfy the conditions above or false otherwise)
 
 """
-function check_initial_room(Δ, a, b)
-    n = length(a)
+function check_initial_room(n, Δ, a, b)
 
     for i = 1:n
         if b[i] < (a[i] + 2.0 * Δ)
